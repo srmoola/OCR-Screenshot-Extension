6b23934ea906b01a47ab676537ebeaa9d374a4e5
Satyadev Moolagani <smoolagani@gmail.com>
2023-12-13 16:58:52 -0500

    Add Split Functionality

--- OCR_Engine/base64_img.py
+++ OCR_Engine/base64_img.py
@@ -5,7 +5,9 @@ import numpy as np
 
 def base64_to_img() -> any:
     with open("image.txt", "r") as f:
-        im_bytes = base64.b64decode(f.read())
+        split_base64 = f.read().partition(",")
+        print(split_base64[0])
+        im_bytes = base64.b64decode(split_base64[2])
 
     im_arr = np.frombuffer(im_bytes, dtype=np.uint8)
     img = cv2.imdecode(im_arr, flags=cv2.IMREAD_COLOR)
